423b6d0df60ac5bbcf40f1efe72c87ae88412fa2
Jon Moss <me@jonathanmoss.me>
2015-12-26 11:34:46 -0500

    Return 404 on Users#show is user cannot be found

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,6 @@ class UsersController < ApplicationController
   private
 
   def set_user
-    @user = User.find_by(username: params[:username])
+    @user = User.find_by!(username: params[:username])
   end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,10 +3,20 @@ require 'rails_helper'
 RSpec.describe UsersController, type: :controller do
 
   describe 'GET #show' do
-    it 'returns http success' do
-      @user = FactoryGirl.create(:user)
-      get :show, username: @user.username
-      expect(response.status).to eq(200)
+    context 'valid user' do
+      it 'returns http 200' do
+        @user = FactoryGirl.create(:user)
+        get :show, username: @user.username
+        expect(response.status).to eq(200)
+      end
+    end
+
+    context 'invalid user' do
+      it 'returns http 404' do
+        expect do
+          get :show, username: 'fakeuser'
+        end.to raise_error(ActiveRecord::RecordNotFound)
+      end
     end
   end
 end
